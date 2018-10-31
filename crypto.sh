4e805bf3b4af2c562486ffb138c79f9840e204dc
xQsme <2151163@my.ipleiria.pt>
2018-10-31 14:08:09 +0000

    mount complete

--- crypto.sh
+++ crypto.sh
@@ -1,4 +1,7 @@
-blkid -c /dev/nul | awk -F: '{print "mkdir "$1}' > mount.sh
-blkid -c /dev/nul | awk -F: '{print "mount "$1" "$1}' >> mount.sh
+echo "mkdir ~/temp" > mount.sh
+echo "mkdir ~/temp/dev" > mount.sh
+blkid -c /dev/nul | awk -F: '{print "mkdir ~/temp"$1}' >> mount.sh
+blkid -c /dev/nul | awk -F: '{print "mount "$1" ~/temp"$1}' >> mount.sh
 chmod +x mount.sh
 ./mount.sh
+crypto-finder -d  ~/temp/dev
