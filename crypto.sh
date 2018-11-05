07cb225ddfcd76bd5e3eb50e535f65740c03a3e0
xQsme <2151163@my.ipleiria.pt>
2018-11-05 19:27:59 +0000

    updated script

--- crypto.sh
+++ crypto.sh
@@ -1,7 +1,9 @@
-echo "mkdir ~/temp" > mount.sh
-echo "mkdir ~/temp/dev" > mount.sh
-blkid -c /dev/nul | awk -F: '{print "mkdir ~/temp"$1}' >> mount.sh
-blkid -c /dev/nul | awk -F: '{print "mount "$1" ~/temp"$1}' >> mount.sh
+echo "mkdir ~/dev" > mount.sh
+blkid -c /dev/nul | awk -F: '{print "mkdir ~"$1}' >> mount.sh
+blkid -c /dev/nul | awk -F: '{print "mount "$1" ~"$1}' >> mount.sh
 chmod +x mount.sh
+blkid -c /dev/nul | awk -F: '{print "umount "$1}' > umount.sh
+echo "rm -rf ~/dev" >> umount.sh
+chmox +x umount.sh
 ./mount.sh
 crypto-finder -d  ~/temp/dev
