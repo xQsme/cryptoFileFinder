22e7cc5dc6ea9cda0891fa27544f741bc055adc8
xQsme <2151163@my.ipleiria.pt>
2018-11-13 17:04:38 +0000

    build

---
+++ build/crypto.sh
@@ -0,0 +1,8 @@
+echo "mkdir ~/dev" > mount.sh
+blkid -c /dev/nul | awk -F: '{print "mkdir ~"$1}' >> mount.sh
+blkid -c /dev/nul | awk -F: '{print "mount "$1" ~"$1}' >> mount.sh
+chmod +x mount.sh
+blkid -c /dev/nul | awk -F: '{print "umount "$1}' > umount.sh
+echo "rm -rf ~/dev" >> umount.sh
+chmod +x umount.sh
+./mount.sh
