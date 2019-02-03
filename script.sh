fbc8dd5f2e56121e4901341864cc3f5bbb7e6670
xQsme <2151163@my.ipleiria.pt>
2019-02-03 00:37:14 +0000

    safer unmount

--- script.sh
+++ script.sh
@@ -1,8 +1,9 @@
 echo "mkdir ~/dev" > mount.sh
 blkid -c /dev/nul | awk -F: '{print "mkdir ~"$1}' >> mount.sh
-blkid -c /dev/nul | awk -F: '{print "mount "$1" ~"$1}' >> mount.sh
+blkid -c /dev/nul | awk -F: '{print "mount -r "$1" ~"$1}' >> mount.sh
 chmod +x mount.sh
 blkid -c /dev/nul | awk -F: '{print "umount "$1}' > umount.sh
-echo "rm -rf ~/dev" >> umount.sh
+blkid -c /dev/nul | awk -F: '{print "rm ~"$1}' >> umount.sh
+echo "rm ~/dev" >> umount.sh
 chmod +x umount.sh
 ./mount.sh
